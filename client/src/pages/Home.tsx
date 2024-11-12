3e7d98a06c005bf8e2630893b3ae1df62a35178e
JustineC31 <justine-cousin@hotmail.com>
2024-11-12 16:33:52 +0100

    feat: create the HTML for the home page

---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,21 @@
+export default function Home() {
+  return (
+    <div>
+      <img
+        src="/src/assets/images/logo-pokemon-arena.png"
+        alt="Logo Pokemon Arena"
+      />
+      <h1>Bienvenue sur Pokémon Arena !</h1>
+      <p>
+        Explorez notre Pokédex complet pour découvrir chaque Pokémon : types,
+        statistiques, évolutions… toutes les informations pour devenir
+        incollable ! Prêt pour le combat ? Choisissez votre Pokémon favori et
+        lancez-vous dans des tournois épiques et personnalisés dans notre arène
+        !
+      </p>
+      <button type="button">button</button>
+      <button type="button">button</button>
+      <footer>Made with ❤ by Justine, Florentin, Rémi et Julien</footer>
+    </div>
+  );
+}
