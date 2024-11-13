ad07ebac9a1e34189009a02f6edf2a77d8e105b4
JustineC31 <justine-cousin@hotmail.com>
2024-11-13 20:39:15 +0100

    feat: creat button + css button

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,5 +1,7 @@
 import "/src/styles/Home.css";
+import imgFirstButton from "/src/assets/images/Pokemon-removebg-preview 1.png";
 import logoPokemon from "/src/assets/images/logo-pokemon-arena.png";
+import imgSecondButton from "/src/assets/images/pokemon-pokedex-button.svg";
 
 export default function Home() {
   return (
@@ -19,8 +21,22 @@ export default function Home() {
             dans des tournois épiques et personnalisés dans notre arène !
           </p>
         </div>
-        <button type="button">button</button>
-        <button type="button">button</button>
+        <button type="button" className="firstButton">
+          Battle
+          <img
+            className="imgFirstButton"
+            src={imgFirstButton}
+            alt="Pokemon Pokeball"
+          />
+        </button>
+        <button type="button" className="secondButton">
+          Pokedex
+          <img
+            className="imgSecondButton"
+            src={imgSecondButton}
+            alt="Pokemon Pokedex"
+          />
+        </button>
       </div>
       <footer>
         Made with <span className="heart">❤</span> by Justine, Florentin, Rémi
