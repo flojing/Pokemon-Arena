9772da339363253174fc63ee9daac4408365419f
JustineC31 <justine-cousin@hotmail.com>
2024-11-13 10:59:17 +0100

    fix: move logo to imports

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -7,6 +7,7 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 // Import the main app component
 import App from "./App";
+import "./main.css";
 
 // Import additional components for new routes
 // Try creating these components in the "pages" folder
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,21 +1,31 @@
+import "/src/styles/Home.css";
+import logoPokemon from "/src/assets/images/logo-pokemon-arena.png";
+
 export default function Home() {
   return (
-    <div>
-      <img
-        src="/src/assets/images/logo-pokemon-arena.png"
-        alt="Logo Pokemon Arena"
-      />
-      <h1>Bienvenue sur Pokémon Arena !</h1>
-      <p>
-        Explorez notre Pokédex complet pour découvrir chaque Pokémon : types,
-        statistiques, évolutions… toutes les informations pour devenir
-        incollable ! Prêt pour le combat ? Choisissez votre Pokémon favori et
-        lancez-vous dans des tournois épiques et personnalisés dans notre arène
-        !
-      </p>
-      <button type="button">button</button>
-      <button type="button">button</button>
-      <footer>Made with ❤ by Justine, Florentin, Rémi et Julien</footer>
+    <div className="home">
+      <div className="homePage">
+        <img src={logoPokemon} alt="Logo Pokemon Arena" />
+        <h1>Bienvenue sur Pokémon Arena !</h1>
+        <div className="paragraph">
+          <p>
+            Explorez notre Pokédex complet pour découvrir chaque Pokémon :
+            types, statistiques, évolutions… toutes les informations pour
+            devenir incollable !
+          </p>
+          <br />
+          <p>
+            Prêt pour le combat ? Choisissez votre Pokémon favori et lancez-vous
+            dans des tournois épiques et personnalisés dans notre arène !
+          </p>
+        </div>
+        <button type="button">button</button>
+        <button type="button">button</button>
+      </div>
+      <footer>
+        Made with <span className="heart">❤</span> by Justine, Florentin, Rémi
+        et Julien
+      </footer>
     </div>
   );
 }
