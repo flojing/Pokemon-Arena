aaed9e1200c0d1f18c4329e550fbd9dbd1d839b5
JustineC31 <justine-cousin@hotmail.com>
2024-11-26 14:46:56 +0100

    feat: make vibration on the pokeball logo

--- client/src/components/Pokeball.tsx
+++ client/src/components/Pokeball.tsx
@@ -1,13 +1,25 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import logoPokemon from "/src/assets/images/logo-pokemon-arena.png";
 import "/src/styles/Pokeball.css";
 
 export default function Pokeball() {
   const [isOpen, setIsOpen] = useState(false);
   const [isAnimationCompleted, setIsAnimationCompleted] = useState(false);
+  const [shouldVibrate, setShouldVibrate] = useState(false);
+
+  useEffect(() => {
+    const vibrateTimeout = setTimeout(() => {
+      if (!isOpen) {
+        setShouldVibrate(true);
+      }
+    }, 3000);
+
+    return () => clearTimeout(vibrateTimeout);
+  }, [isOpen]);
 
   const handleClickLogoOpening = () => {
     setIsOpen(true);
+    setShouldVibrate(false);
     setTimeout(() => {
       setIsAnimationCompleted(true);
     }, 1000);
@@ -24,7 +36,9 @@ export default function Pokeball() {
         onClick={handleClickLogoOpening}
         onKeyDown={handleClickLogoOpening}
         src={logoPokemon}
-        className={`pokeball-logo ${isOpen ? "move-to-header" : ""}`}
+        className={`pokeball-logo ${isOpen ? "move-to-header" : ""} ${
+          shouldVibrate ? "vibrate" : ""
+        }`}
         alt="Logo Pokémon Aréna"
       />
       <div
