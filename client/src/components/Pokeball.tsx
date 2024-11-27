38fc0c83e7d02aaa65a61fff6f90766f2ea45327
JustineC31 <justine-cousin@hotmail.com>
2024-11-27 10:47:06 +0100

    feat: create gif animation

--- client/src/components/Pokeball.tsx
+++ client/src/components/Pokeball.tsx
@@ -2,10 +2,14 @@ import { useEffect, useState } from "react";
 import logoPokemon from "/src/assets/images/logo-pokemon-arena.png";
 import "/src/styles/Pokeball.css";
 
+const DracaufeuGif =
+  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/6.gif";
+
 export default function Pokeball() {
   const [isOpen, setIsOpen] = useState(false);
   const [isAnimationCompleted, setIsAnimationCompleted] = useState(false);
   const [shouldVibrate, setShouldVibrate] = useState(false);
+  const [showHelp, setShowHelp] = useState(false);
 
   useEffect(() => {
     const hasSeenAnimation = localStorage.getItem("pokeballAnimationSeen");
@@ -21,7 +25,16 @@ export default function Pokeball() {
       }
     }, 3000);
 
-    return () => clearTimeout(vibrateTimeout);
+    const helpTimeout = setTimeout(() => {
+      if (!isOpen) {
+        setShowHelp(true);
+      }
+    }, 5000);
+
+    return () => {
+      clearTimeout(vibrateTimeout);
+      clearTimeout(helpTimeout);
+    };
   }, [isOpen]);
 
   const handleClickLogoOpening = () => {
@@ -50,6 +63,15 @@ export default function Pokeball() {
         }`}
         alt="Logo Pokémon Aréna"
       />
+      {showHelp && !isOpen && (
+        <div className="help-container">
+          <img src={DracaufeuGif} alt="Dracaufeu" className="Dracaufeu-gif" />
+          <p className="help-text">
+            Cliquez sur le logo pour rentrer dans l'arène
+          </p>
+        </div>
+      )}
+
       <div
         className={`pokeball-white ${isOpen ? "open" : ""}`}
         hidden={isAnimationCompleted}
