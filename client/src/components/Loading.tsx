dd795bb94f44df0d86e74b90ca13333d82dfb357
Hakkenheiner <remi.zicken@gmail.com>
2024-11-28 11:22:00 +0100

    style: modify detail

--- client/src/components/Loading.tsx
+++ client/src/components/Loading.tsx
@@ -8,7 +8,7 @@ export default function Loading() {
   return (
     <div className="loading-container">
       <h1>Chargement du Pokédex en cours...</h1>
-      <Box sx={{ width: "25vw" }}>
+      <Box sx={{ width: "25vw", minWidth: "300px" }}>
         <LinearProgress variant="determinate" value={progress} />
       </Box>
     </div>
