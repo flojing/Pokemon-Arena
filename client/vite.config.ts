c5b443d8762eace3656ab3698219737344a5e60d
Florentin Monteil <flo.monteil@gmail.com>
2025-05-12 22:13:01 +0200

    deploy config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,21 +1,22 @@
+import path from "node:path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "/src": path.resolve(__dirname, "./src"),
+      // Cela permet d'utiliser à la fois les chemins '/src/...' et '@/...'
+    },
+  },
   server: {
     port: 3000,
   },
   build: {
     outDir: "dist",
     sourcemap: false,
-    // Optimisation pour la production
-    minify: "terser",
-    terserOptions: {
-      compress: {
-        drop_console: true,
-      },
-    },
   },
 });
